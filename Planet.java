e575e74e64f2550e618359b04eeb4d0ef60361ff
MoPaMo <mpm1301@web.de>
2021-08-17 17:47:14 +0200

    add template

---
+++ Planet.java
@@ -0,0 +1,25 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Die einzigen aktiven Akteure in der Roboterwelt sind die Roboter.
+ * Die Welt besteht aus 14 * 10 Feldern.
+ */
+
+public class Planet extends World
+{
+    private static int zellenGroesse = 50;
+
+    /**
+     * Erschaffe eine Welt mit 15 * 12 Zellen.
+     */
+    public Planet()
+    {
+        super(16, 12, zellenGroesse);
+         setBackground("images/boden.png");
+        setPaintOrder(String.class, Rover.class, Marke.class, Gestein.class, Huegel.class);
+        Greenfoot.setSpeed(20); 
+    }
+
+  
+    
+}
